15f2327ad2570795141a717000b1a8c1aab1fb5d
alen <grafuxa@gmail.com>
2021-04-22 14:27:01 +0200

    refactor

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
 import axios from "axios";
 import "regenerator-runtime/runtime";
 const END_POINT = "https://swapi.dev/api/starships/";
-const starships = [];
-function getCatsFactsList(url) {
-  return axios
-    .get(url)
-    .then((res) => {
-      starships.push(...res.data.results);
-      return starships;
-    })
-    .catch((err) => console.log(err, "GotError"));
-}
 
-async function init() {
+// function getCatsFactsList(url) {
+//   return axios
+//     .get(url)
+//     .then((res) => {
+//       return starships;
+//     })
+//     .catch((err) => console.log(err, "GotError"));
+// }
+
+// async function init() {
+//   try {
+//     const {data} = await getCatsFactsList(END_POINT);
+//     console.log(list);
+//     list.forEach(
+//       (e) =>
+//         (document.querySelector("#app").innerHTML += `
+//     <li>${e.name}</li>
+//     `)
+//     );
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// init();
+
+const getStarshipList = async((url) => {
   try {
-    const list = await getCatsFactsList(END_POINT);
-    console.log(list);
-    list.forEach(
-      (e) =>
-        (document.querySelector("#app").innerHTML += `
-    <li>${e.name}</li>
-    `)
-    );
+    const { data } = await.get(url);
+    return data;
   } catch (err) {
-    console.log(err);
+    console.log(err, "ERROR");
   }
+});
+
+
+const renderContent = () =>{
+  const list = await getStarshipList(END_POINT);
+  document.querySelector("#app").innerHTML = list;
 }
 
-init();
+renderContent()

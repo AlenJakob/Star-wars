684ebabbf1e2fe487446224fa36f3c04a4225db3
alen <grafuxa@gmail.com>
2021-04-22 14:09:54 +0200

    get api result

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-(function () {
-    document.querySelector("body").innerHTML = "Welcome Stranger"
-    console.log(this)
-})()
+import axios from "axios";
+import "regenerator-runtime/runtime";
+const END_POINT = "https://swapi.dev/api/starships/";
+const starships = [];
+function getCatsFactsList(url) {
+  return axios
+    .get(url)
+    .then((res) => {
+      starships.push(...res.data.results);
+      return starships;
+    })
+    .catch((err) => console.log(err, "GotError"));
+}
+
+async function init() {
+  try {
+    const list = await getCatsFactsList(END_POINT);
+    console.log(list);
+    list.forEach(
+      (e) =>
+        (document.querySelector("#app").innerHTML += `
+    <li>${e.name}</li>
+    `)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();

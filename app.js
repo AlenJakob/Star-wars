aa0e74bd66ce5779a2dd14cf3ced123e45284d45
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-04-23 06:54:13 +0200

    api

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 import axios from "axios";
 import "regenerator-runtime/runtime";
 const END_POINT = "https://swapi.dev/api/starships/";
+const people = 'https://swapi.dev/api/people/'
+const api = 'https://swapi.dev/api/';
 
 function addToDom(list, dom) {
   for (const e of list) {
     dom.innerHTML += `<li>${e.name}</li>`;
   }
 }
-const getStarshipList = async (url) => {
+const getList = async (url) => {
   try {
     const { data } = await axios.get(url);
+    console.log(Object.keys(data))
+    for(let endPoint of Object.keys(data)){
+      console.log(endPoint)
+    }
     return data.results;
   } catch (err) {
     return err;
   }
 };
 
-const renderContent = async () => {
-  const list = await getStarshipList(END_POINT);
+const renderContent = async (url) => {
+  const list = await getList(url);
   addToDom(list, document.querySelector("#app"));
 };
 
-renderContent();
+
+// renderContent(END_POINT);
+renderContent(api);
